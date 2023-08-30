d864b2b438a773a33d869949adc73e639b151652
Tochukwu <nzewitochukwu26@gmail.com>
2023-08-30 14:21:25 +0100

    setting up pages and componenets

---
+++ src/components/BackgroundImage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BackgroundImage = () => {
+    return (
+        <div>BackgroundImage</div>
+    )
+}
---
+++ src/pages/TvShow.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TvShow = () => {
+    return (
+        <div>TvShow</div>
+    )
+}
