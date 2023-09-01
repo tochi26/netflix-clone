c6ef0ad3f462b256e42b9739f19d5b8b1ca21025
Tochukwu <nzewitochukwu26@gmail.com>
2023-09-01 01:06:35 +0100

    Implement onScroll event

--- src/pages/Netflix.js
+++ src/pages/Netflix.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Netflix = () => {
+
+    const [isScrolled, setisScrolled] = useState(false)
+
+    window.onscroll = () => {
+        setisScrolled(window.pageYOffset === 0 ? false : true)
+        return () => (window.onscroll = null)
+    }
+    console.log(isScrolled)
     return (
-        <div>Netflix</div>
+        <div className='hero'>
+            <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg'
+                alt='hero img'
+            />
+            <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg'
+                alt='hero img'
+            />
+            <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg'
+                alt='hero img'
+            />
+            <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg'
+                alt='hero img'
+            />
+        </div>
     )
 }
 
