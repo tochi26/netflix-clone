b03648cb3ebb287d2c2ebc03224c484e2c439ec8
Tochukwu <nzewitochukwu26@gmail.com>
2023-09-01 13:50:44 +0100

    Implement the TopNav section

---
+++ src/components/TopNav.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import { AiOutlineLogout } from 'react-icons/ai'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const TopNav = (isScrolled) => {
+
+    const navlinks = [
+        { name: 'Home', link: '/' },
+        { name: 'Tv Show', link: '/tv' },
+        { name: 'My List', link: '/myList' },
+        { name: 'Movies', link: '/movies' },
+    ]
+    return (
+        <NavContainer>
+            <nav className={`${isScrolled ? 'scrolled' : 'notScrolled'}`}>
+                <div className="leftSide">
+                    <div className="logo">
+                        <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668265433/NetflixApp/2560px-Netflix_2015_logo.svg_rbicwl_knwp6f.png'
+                            alt='logo'
+                        />
+                    </div>
+                    <ul className='links'>
+                        {
+                            navlinks.map(({ name, link }) => {
+                                return (
+                                    <li key={name}>
+                                        <Link to={link}>{name}</Link>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+
+                <div className="rightSide">
+                    <button>
+                        <AiOutlineLogout />
+                    </button>
+                </div>
+            </nav>
+
+        </NavContainer>
+    )
+}
+
+const NavContainer = styled.div`
+  .notScrolled {
+    display: flex;
+}
+.scrolled {
+    display: flex;
+    background-color: black;
+}
+nav{
+    position: sticky;
+    top: 0;
+    height: 6rem;
+    width: 100%;
+    justify-content: space-between;
+    position: fixed;
+    z-index: 2;
+    padding: 0.4rem;
+    align-items: center;
+    transition: 0.3s ease-in-out;
+    .leftSide{
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+
+    .logo{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    img{
+        width: 10rem;
+        height: 2rem;
+    }
+}
+.links{
+    display: flex;
+    list-style-type:none;
+    gap: 3rem;
+    li{
+        a{
+            color: white;
+            text-decoration: none;
+        }
+    }
+}
+}
+
+.rightSide{
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    button{
+        background-color: red;
+        border: none; 
+        cursor: pointer;
+        border-radius: 50%;
+    }&:focus{
+        outline: none;
+    }svg{
+        color: white;
+        font-size:2rem;
+    }
+}
+`
+export default TopNav 
--- src/pages/Netflix.js
+++ src/pages/Netflix.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react'
+import TopNav from '../components/TopNav'
+
 
 const Netflix = () => {
 
-    const [isScrolled, setisScrolled] = useState(false)
+    const [isScrolled, setIsScrolled] = useState(false)
 
     window.onscroll = () => {
-        setisScrolled(window.pageYOffset === 0 ? false : true)
+        setIsScrolled(window.pageYOffset === 0 ? false : true)
         return () => (window.onscroll = null)
     }
     console.log(isScrolled)
     return (
         <div className='hero'>
+            <TopNav isScrolled={isScrolled} />
             <img src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1668267540/NetflixApp/avengers-age-of-ultron-team-together-poster-wallpaper-1600x600-92751_84_qvwbif.jpg'
                 alt='hero img'
             />
