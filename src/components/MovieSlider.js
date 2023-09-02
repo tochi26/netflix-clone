8299357f2ec9082dea2e98eddb1cb5dfabc1f9fa
Tochukwu <nzewitochukwu26@gmail.com>
2023-09-02 15:02:39 +0100

    Implement MovieSlider

--- src/components/MovieSlider.js
+++ src/components/MovieSlider.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import Card from './Card'
 import styled from 'styled-components'
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 
 export default React.memo(function MovieSlider({ data, title }) {
+
+    const listRef = useRef()
+
+    const [controlVisibility, setControlVisibility] = useState(false)
+    const [sliderPosition, setSliderPosition] = useState(0)
+
+    const handleDirection = (direction) => {
+        let distance = listRef.current.getBoundingClientRect().x - 70;
+        if (direction === 'left' && sliderPosition > 0) {
+            listRef.current.style.transform = `translatex(${230 + distance}px)`
+            setSliderPosition(sliderPosition - 1)
+        }
+        if (direction === 'right' && sliderPosition < 4) {
+            listRef.current.style.transform = `translatex(${-230 + distance}px)`
+            setSliderPosition(sliderPosition + 1)
+        }
+    }
+
+
     return (
-        <Container>
+        <Container
+            controlVisibility={controlVisibility}
+            onMouseEnter={() => setControlVisibility(true)}
+            onMouseLeave={() => setControlVisibility(false)}
+        >
             <h1>{title}</h1>
             <div className='wrapper'>
-                <div className='slider'>
+                <div className={`slider-action left ${!controlVisibility ? 'none' : ''}`}>
+                    <AiOutlineLeft onClick={() => handleDirection('left')} />
+                </div>
+                <div className='slider' ref={listRef}>
                     {data.map((movie, index) => {
                         return <Card movieData={movie} index={index} key={movie.id} />
                     })}
+                </div>
+                <div className={`slider-action right ${!controlVisibility ? 'none' : ''}`}>
+                    <AiOutlineRight onClick={() => handleDirection('right')} />
                 </div>
             </div>
 
@@ -37,6 +67,32 @@ h1{
         transform: translateX(0px);
         transition: 1s ease-in-out;
         margin-left: 5px;
+    }
+    .slider-action{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position:absolute;
+        z-index: 99;
+        height: 100%;
+        top:2rem;
+        bottom: 0;
+        width: 50px;
+        transition: 0.1s ease-in-out;
+        svg{
+            font-size: 2rem;
+            cursor: pointer;
+            color: white;
+        }
+    }
+    .left{
+        left: 0;
+    }
+    .right{
+        right: 0;
+    }
+    .none{
+        display: none;
     }
 }
 `
